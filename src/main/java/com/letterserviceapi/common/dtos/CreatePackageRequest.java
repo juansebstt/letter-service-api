05c1e52cbd0fc8893042e5f41c774f2d048ea4e4
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-16 16:57:36 -0500

    Implemented CreatePackageRequest

--- src/main/java/com/letterserviceapi/common/dtos/CreatePackageRequest.java
+++ src/main/java/com/letterserviceapi/common/dtos/CreatePackageRequest.java
@@ -1,4 +1,22 @@
 package com.letterserviceapi.common.dtos;
 
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreatePackageRequest {
+
+    private Long trackingNumber;
+    private String address;
+    private String content;
+    private Integer weight;
+
+    @Email
+    private String ReceiverEmail;
+    
 }
