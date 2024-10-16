9f86dde3630cb3f353096802be50811e0b32c7c9
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-16 16:43:54 -0500

    Finished PackageModel

--- src/main/java/com/letterserviceapi/common/dtos/PackageModel.java
+++ src/main/java/com/letterserviceapi/common/dtos/PackageModel.java
@@ -1,14 +1,48 @@
 package com.letterserviceapi.common.dtos;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
-@Data
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table
+@Table(name = "packages")
 public class PackageModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long trackingNumber;
+
+    private String recipientName;
+
+    @Email
+    private String recipientEmail;
+
+    private String recipientAddress;
+
+    @Min(15)
+    private float width;
+
+    @Min(15)
+    private float height;
+
+    @Min(15)
+    private float length;
+
+    private float weight;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
 }
