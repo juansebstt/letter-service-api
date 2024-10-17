dc6acde514c273633769bc5225437c43b6f0b285
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-16 19:32:40 -0500

    Updated PackageModel

--- src/main/java/com/letterserviceapi/common/constants/ApiPathConstants.java
+++ src/main/java/com/letterserviceapi/common/constants/ApiPathConstants.java
@@ -1,6 +1,6 @@
 package com.letterserviceapi.common.constants;
 
-public class ApiConstants {
+public class ApiPathConstants {
 
     public static final String V1_ROUTE = "/v1";
     public static final String PACKAGE_ROUTE = "/package";
