05b208461eb692b72521e3099361753665bfa38a
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-27 01:38:01 -0500

    LetterModel partially done

--- src/main/java/com/letterserviceapi/common/entities/LetterModel.java
+++ src/main/java/com/letterserviceapi/common/entities/LetterModel.java
@@ -1,4 +1,44 @@
 package com.letterserviceapi.common.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "letter")
 public class LetterModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long trackingNumber;
+
+    @NotNull(message = "Sender name is required")
+    @NotBlank(message = "Sender name cannot be blank")
+    @Schema(description = "Full name of the letter sender")
+    private String senderName;
+
+    @NotNull(message = "Sender email is required")
+    @NotBlank(message = "Sender email cannot be blank")
+    @Schema(description = "Full name of the letter sender")
+    private String senderEmail;
+
+    @NotNull(message = "Sender phone is required")
+    @NotBlank(message = "Sender phone cannot be blank")
+    @Schema(description = "Full name of the letter sender")
+    private String senderPhoneNumber;
+
+    @NotNull(message = "Sender phone is required")
+    @NotBlank(message = "Sender phone cannot be blank")
+    @Schema(description = "Full name of the letter sender")
+    private String recipientName;
+
+    
+
 }
