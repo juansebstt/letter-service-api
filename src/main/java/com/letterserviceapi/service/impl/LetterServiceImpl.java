cc3bcb27fda442afcc6684443264347509374f52
Juan Sebastian Ibarra <juanseibarra011@gmail.com>
2024-10-28 16:27:19 -0500

    Partially implemented LetterServiceImpl

--- src/main/java/com/letterserviceapi/service/impl/LetterServiceImpl.java
+++ src/main/java/com/letterserviceapi/service/impl/LetterServiceImpl.java
@@ -1,7 +1,31 @@
 package com.letterserviceapi.service.impl;
 
+import com.letterserviceapi.common.dtos.CreateLetterRequest;
+import com.letterserviceapi.common.dtos.CreateLetterResponse;
+import com.letterserviceapi.common.dtos.LetterContentResponse;
+import com.letterserviceapi.common.dtos.UpdateLetterRequest;
+import com.letterserviceapi.service.LetterService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LetterServiceImpl {
+public class LetterServiceImpl implements LetterService {
+    @Override
+    public CreateLetterResponse createLetter(CreateLetterRequest createLetterRequest, Long userId) {
+        return null;
+    }
+
+    @Override
+    public LetterContentResponse getLetterContent(Long trackingNumber) {
+        return null;
+    }
+
+    @Override
+    public void updateLetter(UpdateLetterRequest updateLetterRequest, Long trackingNumber) {
+
+    }
+
+    @Override
+    public void deleteLetter(Long trackingNumber) {
+
+    }
 }
